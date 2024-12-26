8ead1a437a8f7afd3d462764e8a61ead83b05873
wissemih <mobileapp@webinstats.com>
2024-12-26 16:19:17 +0300

    Spm added

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "MySPMProject",
+    platforms: [
+        .iOS(.v13) // Minimum iOS sürümünü burada belirtebilirsiniz
+    ],
+    products: [
+        .library(
+            name: "iOS_wis",
+            targets: ["iOS_wis"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "iOS_wis",
+            path: "./iOS_wis.xcframework"
+        )
+    ]
+)
